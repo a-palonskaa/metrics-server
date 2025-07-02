4701125315babe5afd8f938bdd4c17b61afd4dba
Alina <alinapalonskaa0@gmail.com>
2025-07-02 12:35:39 +0300

    agent refactoring

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,25 +7,24 @@ import (
 
 	ha "github.com/a-palonskaa/metrics-server/internal/handlers/agent"
 	mt "github.com/a-palonskaa/metrics-server/internal/metrics"
-	st "github.com/a-palonskaa/metrics-server/internal/storage"
+	st "github.com/a-palonskaa/metrics-server/internal/metrics_storage"
 )
 
 func main() {
 	memStats := &runtime.MemStats{}
-	metrics := &mt.MetricsStorage{make(map[string]st.Gauge, 3), make(map[string]st.Counter, 1)}
 
-	mt.Update(metrics, memStats)
-	go mt.UpdateRoutine(metrics, memStats)
+	mt.Update(st.MS, memStats)
+	go mt.UpdateRoutine(st.MS, memStats)
 
 	client := &http.Client{}
 	for {
-		for key, val := range metrics.GaugeMetrics {
+		for key, val := range st.MS.GaugeMetrics {
 			if err := ha.SendRequest(client, "gauge", key, val); err != nil {
 				return
 			}
 		}
 
-		for key, val := range metrics.CounterMetrics {
+		for key, val := range st.MS.CounterMetrics {
 			if err := ha.SendRequest(client, "counter", key, val); err != nil {
 				return
 			}
