b6f56d6d1ed35875ab13371a98bf52e9ab8e6541
Alina <alinapalonskaa0@gmail.com>
2025-07-18 17:22:07 +0300

    hash in response

--- internal/handlers/server/hash_wrapper.go
+++ internal/handlers/server/hash_wrapper.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
@@ -9,6 +10,16 @@ import (
 
 	"github.com/rs/zerolog/log"
 )
+
+type hashResponseWriter struct {
+	http.ResponseWriter
+	bufer []byte
+}
+
+func (w *hashResponseWriter) Write(bufer []byte) (int, error) {
+	w.bufer = bufer
+	return w.ResponseWriter.Write(bufer)
+}
 
 func CheckHash(key string) func(fn http.Handler) http.Handler {
 	return func(fn http.Handler) http.Handler {
@@ -26,6 +37,7 @@ func CheckHash(key string) func(fn http.Handler) http.Handler {
 					w.WriteHeader(http.StatusBadRequest)
 					return
 				}
+				r.Body = io.NopCloser(bytes.NewBuffer(body))
 
 				h := hmac.New(sha256.New, []byte(key))
 				h.Write(body)
@@ -36,6 +48,17 @@ func CheckHash(key string) func(fn http.Handler) http.Handler {
 					w.WriteHeader(http.StatusBadRequest)
 					return
 				}
+
+				myWriter := &hashResponseWriter{ResponseWriter: w, bufer: make([]byte, 0)}
+				fn.ServeHTTP(myWriter, r)
+
+				fn.ServeHTTP(myWriter, r)
+
+				h = hmac.New(sha256.New, []byte(key))
+				h.Write(myWriter.bufer)
+				hash := hex.EncodeToString(h.Sum(nil))
+
+				w.Header().Set("HashSHA256", hash)
 			}
 			fn.ServeHTTP(w, r)
 		})
