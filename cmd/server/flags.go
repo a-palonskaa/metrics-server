815ae4c780ea54c2f1415a561bc3df2801a1d97a
Alina <alinapalonskaa0@gmail.com>
2025-07-25 09:16:11 +0300

    main

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,16 +1,65 @@
 package main
 
 import (
-	"github.com/rs/zerolog/log"
+	"net"
+	"os"
+	"strconv"
 
-	logger "github.com/a-palonskaa/metrics-server/pkg/logger"
+	"github.com/rs/zerolog/log"
 )
 
-func main() {
-	logger.InitLogger("logs/info_server.log")
+const (
+	minPort int = 1
+	maxPort int = 65535
+)
 
-	if err := cmd.Execute(); err != nil {
-		log.Error().Err(err)
+type Config struct {
+	EndpointAddr    string `env:"ADDRESS"`
+	StoreInterval   int    `env:"STORE_INTERVAL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	Restore         bool   `env:"RESTORE"`
+	DatabaseAddr    string `env:"DATABASE_DSN"`
+}
+
+var Flags Config
+
+func setFlags(cfg *Config) {
+	if cfg.EndpointAddr != "" {
+		Flags.EndpointAddr = cfg.EndpointAddr
+	}
+
+	if cfg.FileStoragePath != "" {
+		Flags.FileStoragePath = cfg.FileStoragePath
+	}
+
+	if cfg.DatabaseAddr != "" {
+		Flags.DatabaseAddr = cfg.DatabaseAddr
+	}
+
+	if _, exists := os.LookupEnv("RESTORE"); exists {
+		Flags.Restore = cfg.Restore
+	}
+
+	if _, exists := os.LookupEnv("STORE_INTERVAL"); exists {
+		Flags.StoreInterval = cfg.StoreInterval
+	}
+}
+
+func validateFlags() {
+	_, portStr, err := net.SplitHostPort(Flags.EndpointAddr)
+	if err != nil {
+		log.Error().Msgf("invalid address format: %s", err)
+		return
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Error().Msgf("port must be a number: %s", err)
+		return
+	}
+
+	if port < minPort || port > maxPort {
+		log.Error().Msgf("port must be between %d and %d", minPort, maxPort)
 		return
 	}
 }
